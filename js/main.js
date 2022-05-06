1f57703c022d0d492b5781be72332aaf53c45e10
nachoguirado <69021179+nachoguirado@users.noreply.github.com>
2022-05-06 23:38:55 +0200

    Nuevas Funcionalidades
    Nuevas Funcionalidades

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+# Auto detect text files and perform LF normalization
+* text=auto
 let listaPreguntas = [];
 let url ;
 let index = 0 ;
+let areaPreguntas =  document.getElementById("areaPreguntas");
+let textoPregunta = document.getElementById("pregunta");
+let opciones = document.getElementById("opciones");
+let botonSiguiente = document.createElement("button");
+
+
 function Ajax(url, callback) {
     let peticion = new XMLHttpRequest();
     peticion.addEventListener('readystatechange', function() {
@@ -18,7 +26,6 @@ function Ajax(url, callback) {
 }
 function readJson(file) {
     for (let i in file){
-        console.log(file[i].preguntas)
         let pregunta = {
             id : file[i].id,
             pregunta : file[i].pregunta,
@@ -27,15 +34,44 @@ function readJson(file) {
         }
         listaPreguntas.push(pregunta);
     }
-    
+    index  = 0;
+    renderizarPreguntas();
+
 }
+function renderizarPreguntas(){
+    textoPregunta.innerText = listaPreguntas[index].pregunta;
+    opciones.style.display  = "";
+    let html = "";
+    for(let i = 0; i < listaPreguntas[index].opciones.length; i++){
+        html = html +"<label><input type='checkbox' class='options' value='"+ i +"'>"+ listaPreguntas[index].opciones[i] +"</label></br>";
+        opciones.innerHTML = html;
+        for(let option of document.getElementsByClassName("options")){
+            option.addEventListener('change', function(){
+                if(option.checked == true){
+                    listaPreguntas[index].respuesta = option.value;
+                    if(index == listaPreguntas.length - 1){
+                        botonSiguiente.innerHTML = "Enviar Respuestas";
+                        botonSiguiente.addEventListener("click",enviarRespuestas);
+                    }
+                }
+            });
+        }
+    }    
+}
+
 function makeRequest(){
+    document.getElementById("start").style.display = "none";
     url = "preguntas.json";
-    Ajax(url,readJson);/*
-    document.getElementById("areaPreguntas").innerHTML = listaPreguntas[index].pregunta;
-    let botonSiguiente = document.createElement("button");
-    botonSiguiente.addEventListener("click",function(){index++;document.getElementById("areaPreguntas").innerHTML = listaPreguntas[index].pregunta;})
-    document.getElementById("areaPreguntas").appendChild(botonSiguiente);*/
-    console.log(listaPreguntas)
+    Ajax(url,readJson);
+    botonSiguiente.innerHTML = "Siguiente";
+    areaPreguntas.appendChild(botonSiguiente);
+    botonSiguiente.addEventListener("click",function(){
+        index++;
+        renderizarPreguntas();
+        
+    });    
+}
+function enviarRespuestas(){
+    console.log(JSON.stringify(listaPreguntas));
 }
     
